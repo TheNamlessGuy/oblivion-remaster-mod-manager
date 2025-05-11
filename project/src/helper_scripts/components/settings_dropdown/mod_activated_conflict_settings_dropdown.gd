66e595e24070fab115081668c05b7dda4d65766e
TheNamlessGuy <thenamlessguy+github@hotmail.com>
2025-05-11 21:51:49 +0200

    fix: Forgot to remove PROMPT choice from "Mod Activated Conflict" settings dropdown

--- project/src/helper_scripts/components/settings_dropdown/mod_activated_conflict_settings_dropdown.gd
+++ project/src/helper_scripts/components/settings_dropdown/mod_activated_conflict_settings_dropdown.gd
@@ -12,11 +12,6 @@ extends SettingsDropdown
 @export var replace_text: String = ModActivatedConflict.id_to_title(ModActivatedConflict.REPLACE)
 @export_multiline var replace_tooltip: String = ModActivatedConflict.id_to_tooltip(ModActivatedConflict.REPLACE)
 
-@export_subgroup("Prompt")
-@export var prompt_enabled: bool = true
-@export var prompt_text: String = ModActivatedConflict.id_to_title(ModActivatedConflict.PROMPT)
-@export_multiline var prompt_tooltip: String = ModActivatedConflict.id_to_tooltip(ModActivatedConflict.PROMPT)
-
 func _add_items() -> void:
   _dropdown.add_item(deactivate_text)
   _dropdown.set_item_tooltip(ModActivatedConflict.DEACTIVATE, deactivate_tooltip)
@@ -25,7 +20,3 @@ func _add_items() -> void:
   _dropdown.add_item(replace_text)
   _dropdown.set_item_tooltip(ModActivatedConflict.REPLACE, replace_tooltip)
   _dropdown.set_item_disabled(ModActivatedConflict.REPLACE, not replace_enabled)
-
-  _dropdown.add_item(prompt_text)
-  _dropdown.set_item_tooltip(ModActivatedConflict.PROMPT, prompt_tooltip)
-  _dropdown.set_item_disabled(ModActivatedConflict.PROMPT, not prompt_enabled)
